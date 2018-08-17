124d494e5dfd6682351c3034f6e4971ffc6c348d
niteru <niterukun@gmail.com>
2018-08-17 18:12:23 +0700

    Release 1.0.1

--- build/EventManager.js
+++ build/EventManager.js
@@ -1,207 +1,144 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.EventManager = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.EventManager = factory());
 }(this, (function () { 'use strict';
 
-  var classCallCheck = function (instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  };
+	var ManagerEvent = function ManagerEvent (type, data) {
+		this.type = type;
+		this.data = data;
+			
+		this.prevented = false;
+	};
+		
+	ManagerEvent.prototype.preventDefault = function preventDefault (value) {
+			if ( value === void 0 ) value = true;
 
-  var createClass = function () {
-    function defineProperties(target, props) {
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-      }
-    }
+		this.prevented = !!value;
+	};
 
-    return function (Constructor, protoProps, staticProps) {
-      if (protoProps) defineProperties(Constructor.prototype, protoProps);
-      if (staticProps) defineProperties(Constructor, staticProps);
-      return Constructor;
-    };
-  }();
+	var EventManager = function EventManager () {
+		this.dispose();
+	};
+		
+	EventManager.prototype._findEventListener = function _findEventListener (type, handler) {
+		if (!this._listeners[type]) { return null; }
+		return this._listeners[type].find(function (listener) { return listener.handler === handler; });
+	};
+		
+	EventManager.prototype.dispatchEvent = function dispatchEvent (type, data) {
+			var this$1 = this;
+			if ( data === void 0 ) data = {};
 
-  var toArray = function (arr) {
-    return Array.isArray(arr) ? arr : Array.from(arr);
-  };
+		var event = new ManagerEvent(type, data);
+			
+		if (type !== '__dispatchEvent__')
+			{ if (!this.dispatchEvent('__dispatchEvent__', { event: event })) { return event; } }
+			
+		if (this._listeners[type]) { this._listeners[type].forEach(function (listener) {
+			if (listener.options.prevent) { event.preventDefault(); }
+			if (listener.options.once) { this$1.removeEventListener(type, listener.handler); }
+			listener.handler.call(listener.options.scope || this$1, event, this$1.removeEventListener.bind(this$1, type, listener.handler));
+		}); }
+			
+		return event;
+	};
+		
+	EventManager.prototype.hasEventListener = function hasEventListener (type, handler) {
+		return !!this._findEventListener(type, handler);
+	};
+		
+	EventManager.prototype.addEventListener = function addEventListener (type, handler, options) {
+			if ( options === void 0 ) options = {};
 
-  var ManagerEvent = function () {
-  	function ManagerEvent(type, data) {
-  		classCallCheck(this, ManagerEvent);
+		if (!this.dispatchEvent('__addListener__', { type: type, handler: handler, options: options })) { return false; }
+			
+		if (!this._listeners[type]) { this._listeners[type] = new Array(); }
+			
+		var listener = this._findEventListener(type, handler);
+		if (listener) { listener.options = options; }
+		else { this._listeners[type].push({ handler: handler, options: options }); }
+			
+		return true;
+	};
+		
+	EventManager.prototype.removeEventListener = function removeEventListener (type, handler) {
+			var this$1 = this;
 
-  		this.type = type;
-  		this.data = data;
+		if (!this.dispatchEvent('__removeListener__', { type: type, handler: handler })) { return false; }
+			
+		if (!this._listeners[type]) { return true; }
+			
+		if (!handler) {
+			handler = type;
+			if (typeof handler == 'function')
+				{ for (var type$1 in this$1._listeners)
+					{ this$1.removeEventListener(type$1, handler); } }
+			else if (typeof type == 'string')
+				{ delete this._listeners[type]; }
+		} else { this._listeners[type] = this._listeners[type].filter(function (listener) { return listener.handler !== handler; }); }
+			
+		return true;
+	};
+		
+	EventManager.prototype.on = function on (type, handler, scope) {
+			var this$1 = this;
+			var assign;
 
-  		this.prevented = false;
-  	}
+		type = type.trim().replace(/\s+/g, ' ');
+			
+		if (type.includes(' ')) {
+			var results = new Object();
+			type.split(' ').forEach(function (type) { return results[type.split('.')[0]] = this$1.on(type, handler, scope); });
+			return results;
+		}
+			
+		var options;
+		(assign = type.split('.'), type = assign[0], options = assign.slice(1));
+			
+		if (typeof handler == 'function') {
+			var once = false,
+				    prevent = false;
+				
+			options.forEach(function (option) {
+				switch (option) {
+					case 'once': once = true; break;
+					case 'prevent': prevent = true; break;
+					default: console.warn('EventManager: Unknown option: ' + option);
+				}
+			});
+				
+			return this.addEventListener(type, handler, { once: once, prevent: prevent, scope: scope });
+		} else {
+			var data = handler;
+			return this.dispatchEvent(type, data);
+		}
+	};
+		
+	EventManager.prototype.off = function off (type, handler) {
+			var this$1 = this;
 
-  	createClass(ManagerEvent, [{
-  		key: "preventDefault",
-  		value: function preventDefault() {
-  			var value = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
+		type = type.trim().replace(/\s+/g, ' ');
+			
+		if (type.includes(' ')) {
+			var results = new Object();
+			type.split(' ').forEach(function (type) { return results[type.split('.')[0]] = this$1.off(type, handler); });
+			return results;
+		}
+			
+		type = type.split('.')[0];
+			
+		return this.removeEventListener(type, handler);
+	};
+		
+	EventManager.prototype.dispose = function dispose () {
+		this._listeners = new Object();
+	};
 
-  			this.prevented = !!value;
-  		}
-  	}]);
-  	return ManagerEvent;
-  }();
+	var version = "1.0.1";
 
-  var EventManager = function () {
-  	function EventManager() {
-  		classCallCheck(this, EventManager);
+	EventManager.version = version;
 
-  		this.dispose();
-  	}
-
-  	createClass(EventManager, [{
-  		key: '_findEventListener',
-  		value: function _findEventListener(type, handler) {
-  			if (!this._listeners[type]) return null;
-  			return this._listeners[type].find(function (listener) {
-  				return listener.handler === handler;
-  			});
-  		}
-  	}, {
-  		key: 'dispatchEvent',
-  		value: function dispatchEvent(type) {
-  			var _this = this;
-
-  			var data = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
-  			var event = new ManagerEvent(type, data);
-
-  			if (type !== '__dispatchEvent__') if (!this.dispatchEvent('__dispatchEvent__', { event: event })) return event;
-
-  			if (this._listeners[type]) this._listeners[type].forEach(function (listener) {
-  				if (listener.options.prevent) event.preventDefault();
-  				if (listener.options.once) _this.removeEventListener(type, listener.handler);
-  				listener.handler.call(listener.options.scope || _this, event, _this.removeEventListener.bind(_this, type, listener.handler));
-  			});
-
-  			return event;
-  		}
-  	}, {
-  		key: 'hasEventListener',
-  		value: function hasEventListener(type, handler) {
-  			return !!this._findEventListener(type, handler);
-  		}
-  	}, {
-  		key: 'addEventListener',
-  		value: function addEventListener(type, handler) {
-  			var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-
-  			if (!this.dispatchEvent('__addListener__', { type: type, handler: handler, options: options })) return false;
-
-  			if (!this._listeners[type]) this._listeners[type] = new Array();
-
-  			var listener = this._findEventListener(type, handler);
-  			if (listener) listener.options = options;else this._listeners[type].push({ handler: handler, options: options });
-
-  			return true;
-  		}
-  	}, {
-  		key: 'removeEventListener',
-  		value: function removeEventListener(type, handler) {
-  			if (!this.dispatchEvent('__removeListener__', { type: type, handler: handler })) return false;
-
-  			if (!this._listeners[type]) return true;
-
-  			if (!handler) {
-  				handler = type;
-  				if (typeof handler == 'function') for (var _type in this._listeners) {
-  					this.removeEventListener(_type, handler);
-  				} else if (typeof type == 'string') delete this._listeners[type];
-  			} else this._listeners[type] = this._listeners[type].filter(function (listener) {
-  				return listener.handler !== handler;
-  			});
-
-  			return true;
-  		}
-  	}, {
-  		key: 'on',
-  		value: function on(type, handler, scope) {
-  			var _this2 = this;
-
-  			type = type.trim().replace(/\s+/g, ' ');
-
-  			if (type.includes(' ')) {
-  				var results = new Object();
-  				type.split(' ').forEach(function (type) {
-  					return results[type.split('.')[0]] = _this2.on(type, handler, scope);
-  				});
-  				return results;
-  			}
-
-  			var options = void 0;
-
-  			var _type$split = type.split('.');
-
-  			var _type$split2 = toArray(_type$split);
-
-  			type = _type$split2[0];
-  			options = _type$split2.slice(1);
-
-
-  			if (typeof handler == 'function') {
-  				var once = false,
-  				    prevent = false;
-
-  				options.forEach(function (option) {
-  					switch (option) {
-  						case 'once':
-  							once = true;break;
-  						case 'prevent':
-  							prevent = true;break;
-  						default:
-  							console.warn('EventManager: Unknown option: ' + option);
-  					}
-  				});
-
-  				return this.addEventListener(type, handler, { once: once, prevent: prevent, scope: scope });
-  			} else {
-  				var data = handler;
-  				return this.dispatchEvent(type, data);
-  			}
-  		}
-  	}, {
-  		key: 'off',
-  		value: function off(type, handler) {
-  			var _this3 = this;
-
-  			type = type.trim().replace(/\s+/g, ' ');
-
-  			if (type.includes(' ')) {
-  				var results = new Object();
-  				type.split(' ').forEach(function (type) {
-  					return results[type.split('.')[0]] = _this3.off(type, handler);
-  				});
-  				return results;
-  			}
-
-  			type = type.split('.')[0];
-
-  			return this.removeEventListener(type, handler);
-  		}
-  	}, {
-  		key: 'dispose',
-  		value: function dispose() {
-  			this._listeners = new Object();
-  		}
-  	}]);
-  	return EventManager;
-  }();
-
-  var version = "1.0.0";
-
-  EventManager.version = version;
-
-  return EventManager;
+	return EventManager;
 
 })));
